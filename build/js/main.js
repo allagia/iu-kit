cd3491fde77599d3c7358655ef364a309b5e01bc
Alla Panova <alla.mixalvna@gmail.com>
2021-07-22 14:31:33 +0300

    добавляет новые элементы

--- build/js/main.js
+++ build/js/main.js
@@ -82,3 +82,45 @@ if (slider4) {
   });
 }
 
+var tabs = document.querySelectorAll('.tabs__item--general');
+var tabsIcon = document.querySelectorAll('.tabs__item--icon');
+var tabsSize = document.querySelectorAll('.tabs__item--size');
+var tabsNav = document.querySelectorAll('.tabs-nav');
+
+
+var addActiveClass = function (array) {
+  if (array) {
+    var removeActiveClass = function () {
+      array.forEach(function (tab) {
+        tab.classList.remove('active');
+      });
+    };
+
+    array.forEach(function (tab) {
+      tab.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        removeActiveClass(array);
+        tab.classList.add('active');
+      });
+    });
+  }
+
+};
+
+tabsNav.forEach(function (item) {
+  var tabsItem = item.querySelectorAll('.tab');
+  addActiveClass(tabsItem);
+});
+
+
+addActiveClass(tabs);
+addActiveClass(tabsIcon);
+addActiveClass(tabsSize);
+
+
+var pages = document.querySelectorAll('.pagination-3__link');
+
+if (pages) {
+  addActiveClass(pages);
+}
+
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
 /** Порядок подключения JS-модулей */
 
 // =require modules/slider.js
+// =require modules/tabs.js
+// =require modules/pagination.js
