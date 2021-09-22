8ef5068c55e11c55f4cebdf21de52ba109aed862
Alla Panova <alla.mixalvna@gmail.com>
2021-09-22 12:10:04 +0300

    добавляет загрузчики

--- build/js/main.js
+++ build/js/main.js
@@ -149,4 +149,29 @@ mainInput.addEventListener('change', function () {
   checkAll(mainInput);
 });
 
-// =require modules/uploader-animation.js
+// анимация (изменение длинны загрузки) происходит после изменений процента загрузки в разметке
+
+// анимация круговой загрузки
+let percentageEntity = document.querySelector('.percentage');
+let textEntity = document.querySelector('.circle');
+
+let percentage = percentageEntity.textContent;
+
+let x = parseInt(percentage.replace(/[^\d]/g, ''));
+
+let attribute = textEntity.getAttribute('stroke-dasharray');
+
+let attributeValue = attribute.substring(0, 2);
+
+if (x !== attributeValue) {
+  textEntity.setAttribute('stroke-dasharray', x + ', 100');
+}
+
+
+// анимация прямой загрузки
+let percent = document.querySelector('.uploader__percent');
+
+let level = document.querySelector('.uploader__level');
+
+level.style.width = percent.textContent + '%';
+
